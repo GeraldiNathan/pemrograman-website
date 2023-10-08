eab44da413e390f623df639f7980b20fe29ebf3f
PiccoloLexy <geraldi.tommysaputra03@gmail.com>
2023-10-08 16:49:35 +0800

    Codelab 1 Done

---
+++ Modul2/Codelab 1/script.js
@@ -0,0 +1,8 @@
+function tambah() {
+  var angka1 = parseFloat(document.getElementById("angka1").value);
+  var angka2 = parseFloat(document.getElementById("angka2").value);
+
+  var hasil = angka1 + angka2;
+
+  alert("Hasil Penjumlahan = " + hasil);
+}
