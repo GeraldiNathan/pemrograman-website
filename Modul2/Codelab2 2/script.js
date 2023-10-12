a5c254cd3f3e861938650eac1d7ef09b11a67085
PiccoloLexy <geraldi.tommysaputra03@gmail.com>
2023-10-12 20:12:23 +0800

    Fix: Some Script in js

--- Modul2/Codelab2 2/script.js
+++ Modul2/Codelab2 2/script.js
@@ -1,24 +1,20 @@
-document
-  .getElementById("daftarButton")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
+document.getElementById("daftarButton").addEventListener("click", function () {
+  var nama = document.getElementById("nama").value;
+  var email = document.getElementById("email").value;
+  var alamat = document.getElementById("alamat").value;
 
-    var nama = document.getElementById("nama").value;
-    var email = document.getElementById("email").value;
-    var alamat = document.getElementById("alamat").value;
-
-    if (nama === "" || email === "" || alamat === "") {
-      alert("Semua data harus diisi.");
-    } else {
-      alert(
-        "Info Detail Data" +
-          "\nNama: " +
-          nama +
-          "\nEmail:" +
-          email +
-          "\nAlamat:" +
-          alamat +
-          "\nData berhasil ditambah :)"
-      );
-    }
-  });
+  if (nama === "" || email === "" || alamat === "") {
+    alert("Semua data harus diisi.");
+  } else {
+    alert(
+      "Info Detail Data" +
+        "\nNama: " +
+        nama +
+        "\nEmail:" +
+        email +
+        "\nAlamat:" +
+        alamat +
+        "\nData berhasil ditambah :)"
+    );
+  }
+});
