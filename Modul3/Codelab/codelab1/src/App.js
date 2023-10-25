8a74ddfe2847bfc1e05af4fe951cadaa638b6c35
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-10-26 01:32:30 +0800

    Feat(Codelab 2): Fix anything

--- Modul3/Codelab/codelab1/src/App.js
+++ Modul3/Codelab/codelab1/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 const Home = () => <h1>Home</h1>;
-const About = () => <h1>About Us</h1>;
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    {/* Content for the About page goes here */}
+  </div>
+);
 const Contact = () => <h1>Contact Us</h1>;
 
 function App() {
   return (
     <Router>
+      <p style={{ fontSize: 35 }}>Navbar</p>
       <nav>
         <ul>
           <li>
@@ -21,11 +27,14 @@ function App() {
           </li>
         </ul>
       </nav>
+      <p>Halaman Abous Us</p>
       <Routes>
         <Route path="/" element={Home} />
         <Route path="/about" element={About} />
         <Route path="/contact" element={Contact} />
       </Routes>
+
+      <p>Footer</p>
     </Router>
   );
 }
