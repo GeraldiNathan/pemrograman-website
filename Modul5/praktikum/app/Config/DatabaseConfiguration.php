fec8540de9db1f62b31a5493811f2a6fb7b36ffa
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-07 22:02:02 +0800

    feat(Pratkikum) : Modul 5 done

---
+++ Modul5/praktikum/app/Config/DatabaseConfiguration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfiguration
+{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "sepatu_web";
+    public $port = 3306;
+}
---
+++ Modul5/praktikum/app/Models/Product.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\Models;
+
+// include "app/Config/DatabaseConfiguration.php";
+
+include "/xampp/htdocs/modul4Web/codelab/app/Config/DatabaseConfiguration.php";
+
+use app\Config\DatabaseConfiguration;
+use mysqli;
+
+class Product extends DatabaseConfiguration
+{
+    public $conn;
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->database_name, $this->port);
+        if ($this->conn->connect_error) {
+            die("Connection Failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function findAll()
+    {
+        $sql = "SELECT products.*, description.price FROM products inner JOIN description on products.id_description = description.id";
+        // $sql = "SELECT * FROM `products`
+        // JOIN description on products.id_description = description.id ";
+        $result = $this->conn->query($sql);
+        $this->conn->close();
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function findById($id)
+    {
+        $sql = "SELECT products.*, description.price FROM products inner JOIN description on products.id_description = description.id WHERE products.id = ? ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $this->conn->close();
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function create($data)
+    {
+        $productName = $data["product_name"];
+        $idDesc = $data["id_description"];
+
+        $sql = "INSERT INTO products (product_name, id_description) VALUES (?, ?)";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("si", $productName, $idDesc);
+        $stmt->execute();
+        $this->conn->close();
+    }
+
+    public function update($data, $id)
+    {
+        $productName = $data["product_name"];
+        $sql = "UPDATE products SET product_name = ? WHERE id = ?   ";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("si", $productName, $id);
+        $stmt->execute();
+        $this->conn->close();
+    }
+    public function delete($id)
+    {
+        $sql = "DELETE FROM products WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $this->conn->close();
+    }
+}
